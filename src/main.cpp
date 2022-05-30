2fc194039e747139bb3625486259a0ec8fd27e0e
AdaSalvadorAvalos <100204789+AdaSalvadorAvalos@users.noreply.github.com>
2022-05-30 09:23:52 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,13 @@
 #include <Arduino.h>
 
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
+
 int led = 13;
 void setup() {
-  // initialize the digital pin as an output.
   pinMode(led, OUTPUT); 
 }
 
 void loop() {
- digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(2000);               // wait for a second
-}
+ digitalWrite(led, HIGH);   
+  delay(1000);               
+  digitalWrite(led, LOW);    
+  delay(2000);              
